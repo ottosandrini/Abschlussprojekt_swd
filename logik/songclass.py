97dfaf15d27877e25ef19a8db52c00edbe87eb15
Jens Nockenberg <nj1693@mci4me.at>
2024-02-02 11:19:57 +0100

    songclass ausgebessert

--- logik/songclass.py
+++ logik/songclass.py
@@ -17,7 +17,7 @@ class Song:
         self.album_art = album_art
         self.song_hash = self.fingerprint_file()
 
-    def fingerprint_file(self.file):
+    def fingerprint_file(self):
         """Generate hashes for a file.
 
         Given a file, runs it through the fingerprint process to produce a list of hashes from it.
@@ -25,10 +25,10 @@ class Song:
         :param filename: The path to the file.
         :returns: The output of :func:`hash_points`.
         """
-        f, t, Sxx = fp.file_to_spectrogram(filename)
+        f, t, Sxx = fp.file_to_spectrogram(self.file)
         peaks = fp.find_peaks(Sxx)
         peaks = fp.idxs_to_tf_pairs(peaks, t, f)
-        return fp.hash_points(peaks, filename)
+        return fp.hash_points(peaks, self.file)
 
     def store_data(self):
         print("... storing data")
